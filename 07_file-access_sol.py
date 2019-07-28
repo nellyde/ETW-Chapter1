8ff92f3229a66dca9d1c70f84e1bbffb09f5c394
nellyde <53389686+nellyde@users.noreply.github.com>
2019-07-28 13:42:54 +0100

    Update 07_file-access_sol.py

--- 07_file-access_sol.py
+++ 07_file-access_sol.py
@@ -4,4 +4,10 @@ for item in file:
     item=item.strip()
     devices.append(item)
 file.close()
-print(devices)
+print(devices
+Character_name="john"
+Character_age="35"
+Print("It's so nice to meet" + character_name+ ",")
+Print("she is" + character_age+ "years old")
+Print("she like the name "+character_name+ ".)
+Print("but she didn't like being" + character_age.")
